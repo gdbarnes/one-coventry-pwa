13016a4cf07f7a877c0d0498a6d1b0e7094b9be3
Greg Barnes <gregdbarnes@gmail.com>
2018-12-19 16:54:15 +0000

    Content for groups page

--- content/welcome-to-one-coventry.md
+++ content/welcome-to-one-coventry.md
@@ -10,4 +10,29 @@ tags:
 date: "2018-12-20"
 ---
 
-Welcome to ONE Coventry
+# Welcome to ONE Coventry.
+
+## Cognoscenti audeat ulterius sublimia terga potiorque saxo
+
+Lorem markdownum, quid fuerat Procne saxo asperitas variarum Iuno humili
+sermone! Tibi Oileos [unda](http://nuncet.org/hic-gens.php), lux celeberrima
+heros inlustre Copia, _humo_: huius.
+
+Ecce ruris resupino pugnat nudaque certum piacula componar advertite homines
+terris: iam. Huc amnem cura lustrabere pro videt ferat lupi quoque tepido
+amplexa aures es subito ad sagittis silvis lacrimans.
+
+## Ad maximus superest illa defendit vias
+
+Penetravit velocius. [Sed usum notissima](http://quamlaevum.com/parnasia) pinus
+monimenta populo dubitavit latices quoque, nido parant? Argumenta regem
+pererrant avellere ore _grates_ stirpem. Toto potes, non malo est argumentum
+gyrum, nec cernes prosunt, [laudemur
+quis](http://www.ipse-in.org/meritosenis.html) ardescunt cervicis. Quamvis mens,
+pastae, vimine, quid urbesque vidi, quae fuit strictis radere grandaevique
+odiumque dixit; confiteorque.
+
+1.  Dryades resupinum
+2.  Lassus vota illos et visis liceret
+3.  Per nec tenaci solitoque peto Neoptolemum vitamque
+4.  Dianae minoris tum
--- src/pages/groups.jsx
+++ src/pages/groups.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import Card from "react-md/lib/Cards/Card";
+import CardText from "react-md/lib/Cards/CardText";
+import {
+  Button,
+  DataTable,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableColumn,
+  FontIcon
+} from "react-md";
 import Helmet from "react-helmet";
 import Layout from "../layout";
-// import Profile from "../components/Profile";
 import config from "../../data/SiteConfig";
 
 class GroupsPage extends Component {
@@ -13,7 +23,35 @@ class GroupsPage extends Component {
             <title>{`Profile | ${config.siteTitle}`}</title>
             <link rel="canonical" href={`${config.siteUrl}/profile/`} />
           </Helmet>
-          {/* <Profile /> */}
+          <Card className="md-grid md-cell--8">
+            <DataTable plain>
+              <TableHeader>
+                <TableRow>
+                  <TableColumn>Groups</TableColumn>
+                  <TableColumn>Status</TableColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow key="joined">
+                  <TableColumn>Staff team</TableColumn>
+                  <TableColumn>
+                    <FontIcon iconClassName="fa fa-check" />{" "}
+                    <span className="md-title">JOINED</span>
+                  </TableColumn>
+                </TableRow>
+                {Array.from(Array(10)).map((_, i) => (
+                  <TableRow key={i}>
+                    <TableColumn>Other group {i + 1}</TableColumn>
+                    <TableColumn>
+                      <Button secondary raised>
+                        Join group
+                      </Button>
+                    </TableColumn>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </DataTable>
+          </Card>
         </div>
       </Layout>
     );
